915e0333f8f7d57bb084aeeeda51c7b5f032c24c
john-rocky <rockyshikoku@gmail.com>
2023-12-27 13:17:05 +0900

    Initial Commit

---
+++ lama-cleaner-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lama-cleaner-ios
+//
+//  Created by 間嶋大輔 on 2023/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lama-cleaner-iosUITests/lama_cleaner_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lama_cleaner_iosUITestsLaunchTests.swift
+//  lama-cleaner-iosUITests
+//
+//  Created by 間嶋大輔 on 2023/12/27.
+//
+
+import XCTest
+
+final class lama_cleaner_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
